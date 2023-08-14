c35372e87de50a01e29a5c844cd95de0fec7e915
izanmaizan <izanmaizan@gmail.com>
2023-08-14 13:16:36 +0700

    update

--- script.js
+++ script.js
@@ -45,5 +45,65 @@ function showTodo(filter)
     taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
 
 }
-
 showTodo("all");
+
+function showMenu(selectedTask) {
+    let menuDiv = selectedTask.parentElement.lastElementChild;
+    menuDiv.classList.add("show");
+    document.addEventListener("click", e => {
+        if(e.target.tagName != "I" || e.target != selectedTask){
+            menuDiv.classList.remove("show");
+        }
+    });
+}
+
+function updateStatus(selectedTask){
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if(selectedTask.checked){
+        taskName = classList.add("checked");
+        todos[selectedTask.id].status = "complete";
+    }else{
+        taskName.classList.remove("checked");
+        todos[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+}
+
+function editTask(taskId, textName){
+    editId = taskId;
+    isEditTask = true;
+    taskInput.value = textName;
+    taskInput.focus();
+    taskInput.classList.add("active");
+}
+
+function deleteTask(deleteId, filter){
+    isEditTask = false;
+    todos.splice(deleteId, 1);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo(filter);
+}
+
+clearAll.addEventListener("click", () =>{
+    isEditTask = false;
+    todos.splice(0, todos.length);
+    localStorage.setItem("todo-list", JSON.stringify(todo));
+    showTodo();
+});
+
+taskInput.addEventListener("keyup", e =>{
+    let userTask = taskInput.value.trim();
+    if(e.key == "Enter" && userTask){
+        if(isEditTask){
+            todos = !todos ? [] : todos;
+            let taskInfo = {Name: userTask, status: "pending"};
+            todos.push(taskInfo);
+        }else{
+            isEditTask = false;
+            todos[editId].name = useTask;
+        }
+        taskInput.value "";
+        localStorage.setItem("todo-list", JSON.stringify(todos));
+        showTodo(document.querySelector("span .active").id); 
+    }
+});
